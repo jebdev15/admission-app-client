821c7f8be95d43829c32dc275ab5e65b447ce30e
tonton <xtian23anthony@gmail.com>
2024-11-27 16:19:37 +0800

    Header UI update

--- src/pages/Student/Header.tsx
+++ src/pages/Student/Header.tsx
@@ -2,11 +2,12 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import chmsuLogo from '../../assets/chmsu.png';
+import './header.css'
 
 export default function ButtonAppBar() {
   return (
     <Box sx={{ 
-        display: {xs: 'colum', md: 'flex'}, 
+        display: {xs: 'flex', md: 'flex'}, 
         alignContent: 'center', 
         alignItems: 'center', 
         justifyContent: 'space-between',
@@ -16,17 +17,27 @@ export default function ButtonAppBar() {
         borderColor: 'primary.main',
         width: '100%', 
         // height: {xs: 'auto', md: '100px'}
-        height: 'auto'
-      }}>
-        <Box sx={{ display: 'flex', flexDirection: {xs: 'column', md: 'row'}, alignContent: 'center', alignItems: 'center' }}>
-          <img src={chmsuLogo} alt="CHMSU Logo" width={50} height={50} />
-          <Typography variant={"h6"} component="div" color='primary' textAlign={'center'}>
+        height: 'auto',
+        backgroundColor: '#fff'
+      }} className='headerContent'>
+        <Box sx={{ display: 'flex', flexDirection: {xs: 'row', md: 'row'}, alignContent: 'center', alignItems: 'center' }}>
+          <img src={chmsuLogo} alt="CHMSU Logo" width={50} height={50} className='logo' />
+          {/* <Typography variant={"h6"} component="div" color='primary' textAlign={'center'}>
               CARLOS HILADO MEMORIAL STATE UNIVERSITY
-          </Typography>
+          </Typography> */}
+          <Typography
+              className="systemName"
+              variant="h6"
+              component="div"
+              sx={{ color: "primary.dark", flexGrow: 1, lineHeight: "1" }}
+            >
+              <span></span>
+              <span></span>
+            </Typography>
         </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignContent: 'center', alignItems: 'center' }}>
-          <Typography variant="h6" color="primary" textAlign={'center'}>ADMISSION SYSTEM</Typography>
-          <Typography variant="body1" component="div" color='primary' textAlign={'center'}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignContent: 'center', alignItems: 'center' }} className='admissionText'>
+          <Typography variant="h6" color="primary" textAlign={'end'} sx={{color: "primary.dark"}}>ADMISSION SYSTEM</Typography>
+          <Typography variant="body2" component="div" color='primary' textAlign={'end'} sx={{ mt: -0.75, color: "primary.dark" }}>
               A.Y 2025-2026
           </Typography>
         </Box>
