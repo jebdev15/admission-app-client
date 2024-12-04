151990baf16e296cd2fe9be0972f60ffbad975eb
tonton <xtian23anthony@gmail.com>
2024-12-04 12:03:13 +0800

    Update ParentProfile.tsx
    same shit as previous, additionally, used sentence cases sa tnan nga input labels..

--- src/pages/Student/Section/ParentProfile/ParentProfile.tsx
+++ src/pages/Student/Section/ParentProfile/ParentProfile.tsx
@@ -58,22 +58,29 @@ const ParentProfile = () => {
                             justifyContent: 'center', 
                             alignItems: 'center',
                             padding: {xs: 2, sm: 4},
-                            gap: 1,
+                            gap: 3,
                             width: '100%',
                         }}
                             onSubmit={handleSubmit}
                     >
-                        <Box>
-                            <School sx={{ color: 'primary.main', fontSize: 50, mb: -1.5}} />
-                            <Work sx={{ color: 'primary.main', fontSize: 50, mb: -1.5}} />
+
+                      <Box sx={{display: 'flex', flexDirection: {xs: 'column-reverse', sm: 'row'}, rowGap: 2, alignItems: 'center', justifyContent: 'space-between', width: '100%'}}>
+                        <Box sx={{display: 'flex', flexDirection: {xs: 'column', sm: 'row'}, columnGap: 1, alignItems: 'center'}}>
+                          <Work sx={{ color: 'primary.main', fontSize: '3rem' }} />
+                          <Typography variant="h6" color="primary">Parent Profile</Typography>
                         </Box>
-                      <Typography variant="body1" color="primary" sx={{ mb: 2 }}>Parent Profile</Typography>
+                        <Box sx={{display: 'flex', flexDirection: 'column', gap: 0, alignItems: {xs: 'center', sm: 'flex-end'}}}>
+                          <Typography variant="body1" color='textSecondary' sx={{fontWeight: 'bold'}}>CHMSU Admission Portal</Typography>
+                          <Typography variant="body1" color='textSecondary' sx={{fontWeight: 'bold'}}>Academic Year 2025 - 2026</Typography>
+                        </Box>
+                      </Box>
+
                         <Grid container size={12} rowSpacing={3} columnSpacing={2} sx={{width: "100%"}}>
                             <FormControl fullWidth>
-                                <InputLabel id="label-select-father_highest_educational_attainment">Father's Highest Educational Attainment</InputLabel>
+                                <InputLabel id="label-select-father_highest_educational_attainment">Father's highest educational attainment</InputLabel>
                                 <Select
                                     labelId="label-select-father_highest_educational_attainment"
-                                    label="Father's Highest Educational Attainment"
+                                    label="Father's highest educational attainment"
                                     id="select-father_highest_educational_attainment"
                                     name="father_highest_educational_attainment"
                                     value={parentProfile.father_highest_educational_attainment}
@@ -92,7 +99,7 @@ const ParentProfile = () => {
                                 <TextField
                                     id="father_occupation"
                                     name="father_occupation"
-                                    label="Father's Occupation"
+                                    label="Father's occupation"
                                     value={parentProfile.father_occupation}
                                     onChange={handleChange}
                                     required
@@ -100,10 +107,10 @@ const ParentProfile = () => {
                                 />
                             </FormControl>
                             <FormControl fullWidth>
-                                <InputLabel id="label-select-mother_highest_educational_attainment">Mother's Highest Educational Attainment</InputLabel>
+                                <InputLabel id="label-select-mother_highest_educational_attainment">Mother's highest educational attainment</InputLabel>
                                 <Select
                                     labelId="label-select-mother_highest_educational_attainment"
-                                    label="Mother's Highest Educational Attainment"
+                                    label="Mother's highest educational attainment"
                                     id="select-mother_highest_educational_attainment"
                                     name="mother_highest_educational_attainment"
                                     value={parentProfile.mother_highest_educational_attainment}
@@ -122,7 +129,7 @@ const ParentProfile = () => {
                                 <TextField
                                     id="mother_occupation"
                                     name="mother_occupation"
-                                    label="Mother's Occupation"
+                                    label="Mother's occupation"
                                     value={parentProfile.mother_occupation}
                                     onChange={handleChange}
                                     required
@@ -130,10 +137,10 @@ const ParentProfile = () => {
                                 />
                             </FormControl>
                             <FormControl fullWidth>
-                                <InputLabel id="label-select-withGuardian">Are you living with a Guardian</InputLabel>
+                                <InputLabel id="label-select-withGuardian">Are you living with a guardian?</InputLabel>
                                 <Select
                                     labelId="label-select-is_living_with_guardian"
-                                    label="Are you living with a Guardian"
+                                    label="Are you living with a guardian?"
                                     id="select-is_living_with_guardian"
                                     name="is_living_with_guardian"
                                     value={parentProfile.is_living_with_guardian}
