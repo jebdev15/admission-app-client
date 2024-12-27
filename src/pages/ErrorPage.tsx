5785b328e34e6b1334e4c4b69dd4db575ee9300c
Jocc22 <johnericblancia@gmail.com>
2024-12-27 17:08:32 +0800

    update src\pages\ErrorPage.tsx
    change the page not found to server is busy

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -6,8 +6,8 @@ const ErrorPage: React.FC = () => {
     const navigate = useNavigate()
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2, justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-            <Typography variant="h1" color="initial" sx={{ fontWeight: 'bold' }}>Page Not Found</Typography>
-            <Typography variant="h5" color="initial">The page you're looking for doesn't exist.</Typography>
+            <Typography variant="h1" color="initial" sx={{ fontWeight: 'bold' }}>Server is busy</Typography>
+            <Typography variant="h5" color="initial">The system is currently processing volume of requests.</Typography>
             <Button variant="text" color="primary" onClick={() => navigate('/')}>
                 Click here to go back to Home
             </Button>
